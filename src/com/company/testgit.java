2511c62e1bd96b88401c937cc5d1b736947aa4a6
wanan <1840412810@qq.com>
2023-03-02 14:11:13 +0800

    second commit hot-fix

--- src/com/company/testgit.java
+++ src/com/company/testgit.java
@@ -4,6 +4,7 @@ public class testgit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello github");
+        System.out.println("hello hot-fix");
 
     }
 }
